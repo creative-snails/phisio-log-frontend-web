b0f66fc4f6418404e04ae96fbd99c609426fdd0c
Emmet <193012977+emmetbworking@users.noreply.github.com>
2025-05-20 16:35:05 +0100

    3-configure-typescript-for-project-standards

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
-import prettier from 'eslint-plugin-prettier'
-import react from 'eslint-plugin-react'
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default tseslint.config(
-  { ignores: [  
+  {
+    ignores: [
       'node_modules',
       'dist',
       'dist-ssr',
@@ -29,10 +30,10 @@ export default tseslint.config(
       '*.njsproj',
       '*.sln',
       '*.sw?',
-  ] 
-},
+    ],
+  },
   {
-     extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -47,17 +48,17 @@ export default tseslint.config(
       'simple-import-sort': simpleImportSort,
     },
     rules: {
-    'no-restricted-imports': [
-    'error',
-  {
-    patterns: [
-      {
-        group: ['../*', '../*/*', '../../*'],
-        message: 'Please use absolute paths with the ~ prefix instead of relative parent paths.'
-      }
-    ]
-   }
-  ],
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['../*', '../*/*', '../../*'],
+              message: 'Please use absolute paths with the ~ prefix instead of relative parent paths.',
+            },
+          ],
+        },
+      ],
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
