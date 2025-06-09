8f83f4911bde8630df88bcc9b0c4c25038507e36
sttadic <stjepan.tadich@gmail.com>
2025-06-09 17:42:02 +0100

    Expand ChatWidget component structure with body and footer sections

--- src/components/chat/ChatWidget.tsx
+++ src/components/chat/ChatWidget.tsx
@@ -1,13 +1,42 @@
+import { FaUserDoctor } from "react-icons/fa6";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+
 const ChatWidget = () => {
   return (
     <div className="container">
       <div className="chat-popup">
+        {/* Chat Header */}
         <div className="chat-header">
           <div className="header-info">
+            <FaUserDoctor /> {/* placeholder logo */}
             <h2 className="logo-text">PhisioLog Assistant</h2>
           </div>
+          <button className="arrow">
+            <SlArrowDown />
+          </button>
         </div>
-        <div className="chat-body"></div>
+
+        {/* Chat Body */}
+        <div className="chat-body">
+          <div className="message bot-message">
+            <FaUserDoctor />
+            <p className="message-text">
+              Hey there!!! <br /> How can I help you today?
+            </p>
+          </div>
+          <div className="message bot-message">
+            <p className="message-text">Great, thanks for asking. Who am I chatting with?</p>
+          </div>
+        </div>
+
+        {/* Chat Footer */}
+        <div className="chat-footer"></div>
+        <form action="#" className="chat-form">
+          <input type="text" placeholder="Message..." className="message-input" required />
+          <button className="arrow">
+            <SlArrowUp />
+          </button>
+        </form>
       </div>
     </div>
   );
