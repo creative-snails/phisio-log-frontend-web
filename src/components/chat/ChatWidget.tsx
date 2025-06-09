bf1394f479de3c35e5b6e5e8e31bc8874565db8e
sttadic <stjepan.tadich@gmail.com>
2025-06-09 16:51:13 +0100

    Add ChatWidget component structure and include react-icons dependency

--- src/components/chat/ChatWidget.tsx
+++ src/components/chat/ChatWidget.tsx
@@ -1,5 +1,16 @@
 const ChatWidget = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="chat-popup">
+        <div className="chat-header">
+          <div className="header-info">
+            <h2 className="logo-text">PhisioLog Assistant</h2>
+          </div>
+        </div>
+        <div className="chat-body"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatWidget;
