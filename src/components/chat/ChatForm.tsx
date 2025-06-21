d0e4040f6c49d0a2e9653932d6968b890127672c
sttadic <stjepan.tadich@gmail.com>
2025-06-21 18:02:43 +0100

    Implement ChatForm submission handling and input reference

--- src/components/chat/ChatForm.tsx
+++ src/components/chat/ChatForm.tsx
@@ -1,9 +1,24 @@
+import React, { useRef } from "react";
 import { IoMdArrowUp } from "react-icons/io";
 
 const ChatForm = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!inputRef.current?.value) return;
+
+    const userMessage = inputRef.current.value.trim();
+
+    inputRef.current.value = "";
+    inputRef.current.focus();
+    console.log(userMessage);
+  };
+
   return (
-    <form action="#" className="chat-form">
-      <input type="text" placeholder="Message..." className="message-input" required />
+    <form action="#" className="chat-form" onSubmit={handleFormSubmit}>
+      <input ref={inputRef} type="text" placeholder="Message..." className="message-input" required />
       <button>
         <IoMdArrowUp className="btn-arrow" />
       </button>
